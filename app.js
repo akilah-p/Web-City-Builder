6972116d67c2a0af7a266b75c6c36ec9e3c52320
Akilah Powell <atpjun@gmail.com>
2022-11-03 18:36:21 -0400

    changed all pics to end jpeg and finished DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 /* Imports */
 
 /* Get DOM Elements */
+const locationSelect = document.getElementById('location-select');
+const locationImage = document.getElementById('location-img');
+
+const architectureSelect = document.getElementById('architecture-select');
+const architectureImage = document.getElementById('architecture-img');
+
+const popularitySelect = document.getElementById('popularity-select');
+const popularityImage = document.getElementById('popularity-img');
+
+const sloganBtn = document.getElementById('slogan-btn');
+const sloganInput = document.getElementById('slogan-input');
+const sloganList = document.getElementById('slogan-list');
 
 /* State */
 
